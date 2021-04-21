47ec124b885d5ff9bd8b9bdcab748d0ddd9a73ba
يونس التركي <younes.alturkey@tuwaiq.edu.sa>
2021-04-21 18:35:29 +0300

    Update Tokenizer.cs

--- TokenizerApp/Components/Tokenizer.cs
+++ TokenizerApp/Components/Tokenizer.cs
@@ -14,17 +14,24 @@ using System.Collections.Generic;
 
 namespace TokenizerApp
 {
-	public class Tokenizer
-	{
-		public List<Token> tokens;
-		public bool enableHistory;
-		public Input input;
-
-		public Tokenizer(string source, Tokenizable[] handlers) { this.input = new Input(source); }
-		public Tokenizer(Input source, Tokenizable[] handlers) { this.input = source; }
-
-		public Token tokenize() { return null; }
-
-		public List<Token> all() { return null; }
-	}
+    public class Tokenizer {
+        public List<Token> tokens;
+        public bool enableHistory;
+        public Input input;
+        public Tokenizable[] handlers;
+        public Tokenizer(string source, Tokenizable[] handlers) {
+            this.input = new Input(source);
+            this.handlers = handlers;
+        }
+        public Tokenizer(Input source, Tokenizable[] handlers) {
+            this.input = source;
+            this.handlers = handlers;
+        }
+        public Token tokenize() {
+            foreach(var handler in this.handlers)
+                if (handler.tokenizable(this)) return handler.tokenize(this);
+            return null;
+        }
+        public List<Token> all() { return null; }
+    }
 }
